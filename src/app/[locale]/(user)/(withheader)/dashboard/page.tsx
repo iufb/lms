867ec8d250ab460f17134c9c39f3c52d9ca20de9
iufb <bashirov3ld2@gmail.com>
2025-04-13 17:48:59 +0500

    dashboard

--- src/app/[locale]/(user)/(withheader)/dashboard/page.tsx
+++ src/app/[locale]/(user)/(withheader)/dashboard/page.tsx
@@ -1,4 +1,17 @@
+import { Profile } from "@/widgets/Profile";
+import { Metadata, ResolvingMetadata } from "next";
+import { getTranslations } from "next-intl/server";
 
+export async function generateMetadata(
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+
+    const t = await getTranslations('profile')
+    return {
+        title: t('meta.title'),
+        description: t('meta.description')
+    }
+}
 export default function DashboardPage() {
-    return <div>dashboard</div>
+    return <Profile />
 }
